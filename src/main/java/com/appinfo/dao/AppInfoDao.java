0bcc262ec19fd785394d24a0bc5f71c670250c02
“Huar <516053976@qq.com>
2018-05-15 21:19:27 +0800

    更新appinfo相关代码

--- src/main/java/com/appinfo/dao/AppInfoDao.java
+++ src/main/java/com/appinfo/dao/AppInfoDao.java
@@ -1,5 +1,22 @@
 package com.appinfo.dao;
 
+import com.appinfo.entity.AppInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface AppInfoDao {
+
+    public List<AppInfo> getAppInfoList(
+            @Param(value = "softwareName") String querySoftwareName,
+            @Param(value = "status") Integer queryStatus,
+            @Param(value = "categoryLevel1") Integer queryCategoryLevel1,
+            @Param(value = "categoryLevel2") Integer queryCategoryLevel2,
+            @Param(value = "categoryLevel3") Integer queryCategoryLevel3,
+            @Param(value = "flatformId") Integer queryFlatformId,
+            @Param(value = "devId") Integer devId,
+            @Param(value = "from") Integer currentPageNo,
+            @Param(value = "pageSize") Integer pageSize)
+            throws Exception;
 
 }
--- src/main/java/com/appinfo/service/AppInfoService.java
+++ src/main/java/com/appinfo/service/AppInfoService.java
@@ -1,10 +1,22 @@
 package com.appinfo.service;
 
+import com.appinfo.entity.AppInfo;
+
+import java.util.List;
+
 /**
  * @author: Huar
  * @create: 2018-05-14 11:03
  **/
 public interface AppInfoService {
 
-
+    public List<AppInfo> getAppInfoList(String querySoftwareName,
+                                        Integer queryStatus,
+                                        Integer queryCategoryLevel1,
+                                        Integer queryCategoryLevel2,
+                                        Integer queryCategoryLevel3,
+                                        Integer queryFlatformId,
+                                        Integer devId,
+                                        Integer currentPageNo,
+                                        Integer pageSize) throws Exception;
 }
